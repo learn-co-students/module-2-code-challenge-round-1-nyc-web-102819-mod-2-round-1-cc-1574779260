d745b456c0443f684a55b2ed328232965b7ace8a
joffitzer <joffitzer@gmail.com>
2019-11-26 10:24:58 -0500

    deliverables before bonus

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index]
   resources :heroines, only: [:index]  
+
+  get '/heroines/new', to: 'heroines#new', as: 'new_heroine'
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+  post '/heroines', to: 'heroines#create'
+
+  get '/power/:id', to: 'powers#show', as: 'power'
 end

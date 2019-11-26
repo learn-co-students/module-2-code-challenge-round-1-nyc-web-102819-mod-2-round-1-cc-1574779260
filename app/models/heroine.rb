1c598b355d76d5f4a82ade1674020c9948103401
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-26 10:59:27 -0500

    deliverables

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,6 @@
 class Heroine < ApplicationRecord
+  belongs_to :power
+
+  validates :super_name, uniqueness: true
+  validates :name, :super_name, :power_id, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+
+  # powers
+  get '/powers', to: "powers#index", as: "powers"
+  get '/powers/:id', to: "powers#show", as: "power"
+  # resources :powers, only: [:index]
+
+  # heroines
+  get '/heroines', to: "heroines#index", as: "heroines"
+  post '/heroines', to: "heroines#create"
+  get '/heroines/new', to: "heroines#new", as: "new_heroine"
+  get '/heroines/:id', to: "heroines#show", as: "heroine"
+  
+  # resources :heroines, only: [:index]  
 end

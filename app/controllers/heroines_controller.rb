26c51414dd2d98bdfbf8e9965ce860e9c50422fc
jlindner22 <jenlindner22@gmail.com>
2019-11-26 11:21:08 -0500

    deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,31 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    power = Power.find(params[:id])
+    # power = Power.where(power_id: @power.id)
+
+  end
+
+  def new
+    @heroine = Heroine.new
+    @power = Power.all
+  end
+
+  def create
+    heroine = Heroine.new(heroine_params)
+    if heroine.save
+      redirect_to heroine_path(heroine)
+    else
+      redirect_to new_heroine_path
+    end
+  end
+
+private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
 end

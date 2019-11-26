f0a9856d649892d59693190003216d333144f294
hdp2112 <hdp2112@columbia.edu>
2019-11-26 10:22:11 -0500

    deliverables

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,30 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      redirect_to new_heroine_path
+    end
+
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+  resources :heroines, only: [:index] 
+  get '/heroines/new', to: 'heroines#new', as: 'new_heroine'
+  post '/heroines', to: 'heroines#create'
+  get '/heroines/:id', to: 'heroines#show', as: 'heroine'
+
+  get '/powers/:id', to: 'powers#show', as: 'power'
 end

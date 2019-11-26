810c9fa9e1b9296db3a37be4c9ee4d00cdc730d6
eliskim27 <eliskim27@gmail.com>
2019-11-26 10:43:40 -0500

    deliverables

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  resources :powers, only: [:index]
-  resources :heroines, only: [:index]  
+#heroines
+  get '/heroines', to: "heroines#index", as: "heroines"
+  get '/heroines/new', to: "heroines#new", as: "new_heroine"
+  post '/heroines', to: "heroines#create"
+  get '/heroines/:id', to: "heroines#show", as: "heroine"
+
+#powers
+  get '/powers', to: "powers#index", as: "powers"
+
+
+  get '/powers/:id', to: "powers#show", as: "power"
 end
